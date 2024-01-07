91ab6a9e7117e1eed5040849eef2037ee8eb3024
saeed-sufi <saeed.sufi@hotmail.com>
2024-01-07 22:13:10 +0330

    Added options for the argv commands separately

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
-let argv = yargs(hideBin(process.argv))
-    .command('add', 'Add a new note.', () => {
-        console.log('Hi there')
-    })
-    .command('remove', 'Remove a note', () => {
-        console.log('Removing a note...')
-    })
-    .command('list', 'List the notes', listNotes)
-    .command('read', 'Read a note', readNote)
-    .help('h')
-    .alias('h', 'help')
-    .parse()
+const addOptions = {
+  title: {
+    describe: 'Note title',
+    demandOption: true,
+    type: 'string'
+  },
+  body: {
+    describe: 'Add your content',
+    demandOption: true,
+    type: 'string'
+  }
+}
 
+let argv = yargs(hideBin(process.argv))
+  .command('add', 'Add a new note.', addOptions, addNote)
+  .command('remove', 'Remove a note', removeNote)
+  .command('list', 'List the notes', listNotes)
+  .command('read', 'Read a note', readNote)
+  .help('h')
+  .alias('h', 'help')
+  .parse()
+
+function addNote(argv) {
+  console.log('Hi there ' + argv.title + argv.body)
+}
+
+function removeNote() {
+  console.log('Removing a note...')
+}
 function listNotes() {
-    console.log('Listing the notes...')
+  console.log('Listing the notes...')
 }
 
 function readNote() {
-    console.log('Reading a note...')
+  console.log('Reading a note...')
 }
